e589f698ad400a239e479c594c5335180456b35e
Hebert do Nascimento <hebertn88@gmail.com>
2024-01-10 20:03:49 -0300

    Criacao metodo updateCustomer no CustomerController

--- src/main/kotlin/edu/hnasc/credit/application/system/controller/CustomerController.kt
+++ src/main/kotlin/edu/hnasc/credit/application/system/controller/CustomerController.kt
@@ -1,6 +1,7 @@
 package edu.hnasc.credit.application.system.controller
 
 import edu.hnasc.credit.application.system.dto.CustomerDto
+import edu.hnasc.credit.application.system.dto.CustomerUpdateDto
 import edu.hnasc.credit.application.system.dto.CustomerView
 import edu.hnasc.credit.application.system.service.impl.CustomerService
 import org.springframework.web.bind.annotation.*
@@ -24,4 +25,15 @@ class CustomerController(
 
     @DeleteMapping("/{id}")
     fun deleteCustomer(@PathVariable id: Long) = customerService.delete(id)
+
+    @PatchMapping
+    fun updateCustomer(
+        @RequestParam(value = "customerId") id: Long,
+        @RequestBody customerUpdateDto: CustomerUpdateDto
+    ): CustomerView {
+        val customer = customerService.findById(id)
+        val customerToUpdate = customerUpdateDto.toEntity(customer)
+        val customerUpdated = customerService.save(customerToUpdate)
+        return CustomerView(customerUpdated)
+    }
 }
