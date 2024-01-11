4621ee74d152159c6b345b0a68674f7436438eeb
Hebert do Nascimento <hebertn88@gmail.com>
2024-01-10 22:02:59 -0300

    Criacao CreditView e CreditViewList. Criacao dos metodos findAllByCustomerId e findByCreditCode no CreditController. Alteracao na CreditService.

--- src/main/kotlin/edu/hnasc/credit/application/system/controller/CreditController.kt
+++ src/main/kotlin/edu/hnasc/credit/application/system/controller/CreditController.kt
@@ -1,11 +1,12 @@
 package edu.hnasc.credit.application.system.controller
 
 import edu.hnasc.credit.application.system.dto.CreditDto
+import edu.hnasc.credit.application.system.dto.CreditView
+import edu.hnasc.credit.application.system.dto.CreditViewList
+import edu.hnasc.credit.application.system.model.Credit
 import edu.hnasc.credit.application.system.service.impl.CreditService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/credits")
@@ -16,5 +17,22 @@ class CreditController(
     fun saveCredit(@RequestBody creditDto: CreditDto): String {
         val credit = creditService.save(creditDto.toEntity())
         return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
+    }
+
+    @GetMapping
+    fun findAllByCustomerId(
+        @RequestParam(value = "customerId") customerId: Long
+    ): List<CreditViewList> =
+        creditService.findAllByCustomer(customerId)
+            .stream()
+            .map { credit: Credit -> CreditViewList(credit) }
+            .toList()
+
+    @GetMapping
+    fun findByCreditCode(@RequestParam(value = "customerId") customerId: Long,
+                         @PathVariable creditCode: UUID
+    ): CreditView {
+        val credit = creditService.findByCreditCode(customerId, creditCode)
+        return CreditView(credit)
     }
 }
