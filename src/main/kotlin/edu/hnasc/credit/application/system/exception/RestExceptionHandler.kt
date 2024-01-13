31a72df47913d3e98cd3f4185f69f0331f67a5bc
Hebert do Nascimento <hebertn88@gmail.com>
2024-01-12 23:14:34 -0300

    Criacao da RestExceptionHandler. Criacao do BusinessException. Refatoracao da camada Service.

---
+++ src/main/kotlin/edu/hnasc/credit/application/system/exception/RestExceptionHandler.kt
@@ -0,0 +1,79 @@
+package edu.hnasc.credit.application.system.exception
+
+import org.springframework.dao.DataAccessException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.validation.ObjectError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.time.LocalDateTime
+
+@RestControllerAdvice
+class RestExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handlerValidException(ex: MethodArgumentNotValidException): ResponseEntity<ExceptionDetails> {
+        val errors: MutableMap<String, String?> = HashMap()
+        ex.bindingResult.allErrors.stream().forEach {
+            error: ObjectError ->
+            val fieldName: String = (error as FieldError).field
+            val messageError: String? = error.defaultMessage
+            errors[fieldName] = messageError
+        }
+        return  ResponseEntity(
+            ExceptionDetails(
+                title = "Bad Request! Consult the documentation",
+                timestamp = LocalDateTime.now(),
+                status = HttpStatus.BAD_REQUEST.value(),
+                exception = ex.javaClass.toString(),
+                details = errors
+            ), HttpStatus.BAD_REQUEST
+        )
+    }
+
+    @ExceptionHandler(DataAccessException::class)
+    fun handlerDataAccessException(ex: DataAccessException): ResponseEntity<ExceptionDetails> {
+        return ResponseEntity
+            .status(HttpStatus.CONFLICT)
+            .body(
+            ExceptionDetails(
+                title = "Bad Request! Consult the documentation",
+                timestamp = LocalDateTime.now(),
+                status = HttpStatus.CONFLICT.value(),
+                exception = ex.javaClass.toString(),
+                details = mutableMapOf(ex.cause.toString() to ex.message)
+            )
+        )
+    }
+
+    @ExceptionHandler(BusinessException::class)
+    fun handlerBusinessException(ex: BusinessException): ResponseEntity<ExceptionDetails> {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+            ExceptionDetails(
+                title = "Bad Request! Consult the documentation",
+                timestamp = LocalDateTime.now(),
+                status = HttpStatus.BAD_REQUEST.value(),
+                exception = ex.javaClass.toString(),
+                details = mutableMapOf(ex.cause.toString() to ex.message)
+            )
+        )
+    }
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handlerIllegalArgumentException(ex: IllegalArgumentException): ResponseEntity<ExceptionDetails> {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+            ExceptionDetails(
+                title = "Bad Request! Consult the documentation",
+                timestamp = LocalDateTime.now(),
+                status = HttpStatus.BAD_REQUEST.value(),
+                exception = ex.javaClass.toString(),
+                details = mutableMapOf(ex.cause.toString() to ex.message)
+            )
+        )
+    }
+}
--- src/main/kotlin/edu/hnasc/credit/application/system/service/impl/CustomerService.kt
+++ src/main/kotlin/edu/hnasc/credit/application/system/service/impl/CustomerService.kt
@@ -1,10 +1,10 @@
 package edu.hnasc.credit.application.system.service.impl
 
+import edu.hnasc.credit.application.system.exception.BusinessException
 import edu.hnasc.credit.application.system.model.Customer
 import edu.hnasc.credit.application.system.repository.CustomerRepository
 import edu.hnasc.credit.application.system.service.ICustomerService
 import org.springframework.stereotype.Service
-import java.lang.RuntimeException
 
 @Service
 class CustomerService(
@@ -12,7 +12,10 @@ class CustomerService(
 ): ICustomerService {
     override fun save(customer: Customer): Customer = customerRepository.save(customer)
     override fun findById(id: Long): Customer = customerRepository.findById(id).orElseThrow {
-        throw RuntimeException("Id $id not found") }
+        throw BusinessException("Customer Id $id not found") }
 
-    override fun delete(id: Long): Unit = customerRepository.deleteById(id)
+    override fun delete(id: Long): Unit {
+        val customer = this.findById(id)
+        customerRepository.delete(customer)
+    }
 }
